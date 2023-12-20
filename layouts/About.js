16eff3acceb1776e88294460e928dcdd34ca432d
Simon Pepin <simon.pepin.1@ens.etsmtl.ca>
2023-12-20 15:06:57 -0500

    modifications mineurs dans les sections footer about et la page principale

--- layouts/About.js
+++ layouts/About.js
@@ -2,7 +2,7 @@ import { markdownify } from "@lib/utils/textConverter";
 
 function About({ data }) {
   const { frontmatter } = data;
-  const { title, mission, vision, values, valueTitle } = frontmatter;
+  const { title, mission, vision, values, valueTitle, teamTitle, team } = frontmatter;
   return (
     <div>
       <section className="section">
@@ -30,7 +30,7 @@ function About({ data }) {
           <div className="section row  -mt-2">
             {mission.map((mission, index) => (
               <div key={index} className=" mt-2">
-                <div className="feature-card bg-white  p-12 shadow border rounded-xl">
+                <div className="feature-card bg-white p-12 border rounded-xl">
                   <div className=" relative">
                     {markdownify(
                       mission.title,
@@ -56,12 +56,33 @@ function About({ data }) {
             {markdownify(valueTitle, "h1", "text-center font-normal")}
             {values.map((values, index) => (
               <div key={index} className="col-6 mt-2 md:col-6">
-                <div className="bg-white p-12 shadow border feature-card rounded-xl full-height">
+                <div className="feature-card bg-white p-12 border rounded-xl full-height">
                   <div className="faq-head relative">
                     {markdownify(values.title, "h4")}
                   </div>
                   {markdownify(
                     values.answer,
+                    "p",
+                    "faq-body mt-4 text-justify"
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container ">
+          <div className="section row -mt-2">
+            {markdownify(teamTitle, "h1", "text-center font-normal")}
+            {team.map((team, index) => (
+              <div key={index} className="col-6 mt-2 md:col-6">
+                <div className="feature-card bg-white p-12 border rounded-xl full-height">
+                  <div className="relative text-center">
+                    {markdownify(team.name, "h4")}
+                  </div>
+                  {markdownify(
+                    team.bio,
                     "p",
                     "faq-body mt-4 text-justify"
                   )}
