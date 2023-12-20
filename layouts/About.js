d49289d794f4230a0ac8d58f2084b4c571912dab
olivierpoupier <olivier@poupier.net>
2023-12-20 15:42:48 -0500

    fix about page

--- layouts/About.js
+++ layouts/About.js
@@ -55,7 +55,7 @@ function About({ data }) {
           <div className="section row -mt-2 bg-theme-light">
             {markdownify(valueTitle, "h1", "text-center font-normal")}
             {values.map((values, index) => (
-              <div key={index} className="col-6 mt-2 md:col-6">
+              <div key={index} className="col-12 md:col-6 mt-2">
                 <div className="feature-card bg-white p-12 border rounded-xl full-height">
                   <div className="faq-head relative">
                     {markdownify(values.title, "h4")}
@@ -76,7 +76,7 @@ function About({ data }) {
           <div className="section row -mt-2">
             {markdownify(teamTitle, "h1", "text-center font-normal")}
             {team.map((team, index) => (
-              <div key={index} className="col-6 mt-2 md:col-6">
+              <div key={index} className="col-12 md:col-6 mt-2">
                 <div className="feature-card bg-white p-12 border rounded-xl full-height">
                   <div className="relative text-center">
                     {markdownify(team.name, "h4")}
