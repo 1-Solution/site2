6af60d02607a50d58728dc8ed3983ca14251ec5a
Jean-Christophe Viau <jean-christophe@1-solution.ca>
2024-12-10 17:32:27 -0500

    Update career profiles with enhanced descriptions and required skills for Developer roles

--- content/careers.fr.md
+++ content/careers.fr.md
@@ -3,11 +3,31 @@ title: Carrière
 layout: careers
 profiles:
 - title: Développeur Analyste
-  description: Tu désires te développer dans un environnement décontracté, où l'évolution de ta carrière fait partie du succès de notre startup. Tu as un DEC ou plus avec 2 ans d'expérience, ou l'équivalent. Nous avons besoin de toi pour participer à différents projets (C#, Javascript, Java, Ruby). Tu es satisfait quand le travail est bien fait. La rigueur est importante pour toi. L'intégrité et l'honnêteté font partie de tes valeurs. Tu auras à travailler en équipe. Tu auras à démontrer une certaine autonomie. Si cela te convient, nous serons heureux de te parler!
+  description: Tu désires te développer dans un environnement décontracté, où l'évolution de ta carrière fait partie du succès de notre startup. Tu as un DEC ou plus avec 3 ans d'expérience, ou l'équivalent. Nous avons besoin de toi pour participer à différents projets (C#, Javascript, Java, Ruby). Tu es satisfait quand le travail est bien fait. La rigueur est importante pour toi. L'intégrité et l'honnêteté font partie de tes valeurs. Tu auras à travailler en équipe. Tu auras à démontrer une certaine autonomie. Si cela te convient, nous serons heureux de te parler!
+  skills:
+  - skill: Diplôme d’études collégiales (DEC) ou plus.
+  - skill: Minimum de 3 ans d’expérience pertinente ou équivalent.
+  - skill: "Connaissance d'une des langages de programmation : C#, JavaScript, Java, ou Ruby."
+  - skill: Bonne compréhension des principes de développement logiciel et des méthodologies.
+  - skill: Expérience de travail dans un environnement Agile
 - title: Développeur IaC
   description: Tu veux mettre tes compétences en avant dans un environnement stimulant et décontracté, où ta progression professionnelle est un moteur de notre succès. Tu as un DEC ou plus avec au moins 2 ans d'expérience en Infrastructure as Code (IaC), notamment avec Terraform, ou l'équivalent. Nous recherchons quelqu’un comme toi pour collaborer sur des projets passionnants impliquant Terraform et d’autres outils d’automatisation du cloud (AWS, Azure, GCP). Tu es fier quand l’infrastructure déployée est robuste, scalable et efficace. La rigueur et l’attention aux détails sont essentielles pour toi. Tu valorises l’intégrité, l’honnêteté et la collaboration. Travailler en équipe, tout en montrant une autonomie impressionnante, fait partie de ta zone de confort. Si tu te reconnais dans cette description, on a hâte de te rencontrer!
+  skills:
+    - skill: Diplôme d’études collégiales (DEC) ou plus.
+    - skill: Minimum de 2 ans d’expérience en Infrastructure as Code (IaC), notamment avec Terraform, ou expérience équivalente.
+    - skill: Connaissance approfondie d’au moins une plateforme cloud (AWS, Azure, ou GCP).
+    - skill: Maîtrise des outils d’automatisation et de configuration tels que Ansible, Puppet, ou Chef. (Atout)
+    - skill: Rigueur et souci du détail dans le déploiement d’infrastructures robustes et scalables.
+    - skill: Expérience de travail dans un environnement Agile
 - title: Développeur Front-End
   description: Tu veux évoluer dans un environnement dynamique où ta créativité et ton expertise sont valorisées. Avec au moins 3 ans d’expérience en développement Frontend, tu maîtrises Angular et/ou React. Tu es passionné par la création d’interfaces utilisateur modernes, intuitives et performantes. Tu seras impliqué dans la conception et le développement de projets innovants, où la qualité du code, l’accessibilité et l’expérience utilisateur sont primordiales. Si tu apprécies les environnements collaboratifs où tes idées comptent, et que tu as un œil pour les détails sans perdre de vue la vision globale, nous voulons te rencontrer!
+  skills:
+    - skill: Diplôme d’études collégiales (DEC) ou plus.
+    - skill: Minimum de 3 ans d’expérience en développement Frontend.
+    - skill: Maîtrise des frameworks Angular et/ou React.
+    - skill: "Solide compréhension des technologies de base : HTML5, CSS3, JavaScript (ES6+)."
+    - skill: Expérience dans la création d’interfaces utilisateur modernes, intuitives et performantes.
+    - skill: Expérience de travail dans un environnement Agile
 perk_title: Les Avantages
 perks:
   - perk: En faveur d'horaires flexibles
--- layouts/Careers.js
+++ layouts/Careers.js
@@ -6,7 +6,7 @@ function Careers({ data }) {
   const { title, profiles, cv, perks, perk_title, apply_now, fields } = frontmatter;
   const { name, email, selected_file, message, submit, upload_cv } = fields;
   const [selectedFile, setSelectedFile] = useState(null);
-
+  const [activeProfile, setActiveProfile] = useState(null);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -14,108 +14,178 @@ function Careers({ data }) {
       setSelectedFile(file.name);
     }
   };
+
+  const closePopup = () => setActiveProfile(null);
+
   return (
     <div>
-      <section className="career-section">
+      <section className="career-section section">
         <div className="container">
-          {markdownify(title, "h1", "text-center font-normal")}
-          <div className="section no-padding-bottom row -mt-4">
-            {profiles.map((profile, index) => (
-              <div key={index} className="mt-4">
-                <div className="bg-white p-12 shadow feature-card rounded-xl">
-                  <div className="relative">
-                    {markdownify(
-                      profile.title,
-                      "h2",
-                      "text-center",
-                      "font-normal"
-                    )}
-                  </div>
-                  {markdownify(profile.description, "p", "mt-4 text-justify")}
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="bg-white p-12 shadow feature-card rounded-xl flex flex-col justify-center items-center mt-4">
-            <div className="relative">
-              {markdownify(
-                perk_title,
-                "h2",
-                "text-center",
-                "font-normal")}
-            </div>
-            <ul className="career-ul p-8">
-              {perks.map((perk, index) => (
-                <li key={index} className="p8">
-                  {markdownify(
-                    perk.perk,
-                    "text-center",
-                    "font-normal"
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="bg-white p-12 shadow feature-card rounded-xl mt-4">
-            <div className="relative mb-4">
-              {markdownify(apply_now, "h2", "text-center")}
-            </div>
-            <form
-              className="flex flex-col gap-4"
-              method="POST"
-              data-netlify="true"
-              name="Career"
-              encType="multipart/form-data"
-            >
-              <input type="hidden" name="form-name" value="Career" />
-              <input
-                type="text"
-                name="name"
-                placeholder={name}
-                className="p-4 border border-gray-300 rounded-md"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder={email}
-                className="p-4 border border-gray-300 rounded-md"
-                required
-              />
-              <textarea
-                placeholder={message}
-                className="p-4 border border-gray-300 rounded-md"
-                rows="4"
-                name="message"
-                required
-              />
-              <label className="flex items-center justify-center p-4 border border-gray-300 rounded-md text-lg font-semibold text-gray-700 cursor-pointer bg-gray-100 hover:bg-gray-200 transition">
-                {upload_cv}
-                <input
-                  type="file"
-                  name="cv"
-                  required
-                  hidden
-                  onChange={handleFileChange}
-                  aria-label={selected_file}
-                />
-              </label>
-              {/* Display selected file name */}
-              {selectedFile && (
-                <p className="text-gray-600 text-sm mt-2">{selected_file}: {selectedFile}</p>
-              )}
-              <button
-                type="submit"
-                className="btn btn-primary"
-              >
-                {submit}
-              </button>
-            </form>
-          </div>
-          <div className="relative p-12">
-          </div>
+          {markdownify(title, "h1", "text-center font-normal" )}
+
+          <ProfilesSection
+            profiles={profiles}
+            setActiveProfile={setActiveProfile}
+          />
+
+          {activeProfile && (
+            <ProfilePopup profile={activeProfile} onClose={closePopup} />
+          )}
+
+          <PerksSection perkTitle={perk_title} perks={perks} />
+
+          <ApplicationForm
+            applyNow={apply_now}
+            fields={{ name, email, selected_file, message, submit, upload_cv }}
+            selectedFile={selectedFile}
+            handleFileChange={handleFileChange}
+          />
         </div>
       </section>
+    </div>
+  );
+}
+
+function ProfilesSection({ profiles, setActiveProfile }) {
+  return (
+    <div className="section no-padding-bottom row -mt-4 grid grid-cols-12">
+      {profiles.map((profile, index) => (
+        <ProfileCard
+          key={index}
+          profile={profile}
+          onClick={() => setActiveProfile(profile)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function ProfileCard({ profile, onClick }) {
+  return (
+    <div 
+      className="mt-4 col-span-4 cursor-pointer"
+      onMouseEnter={(e) => { 
+        e.currentTarget.style.transform = "scale(1.1)"
+        e.currentTarget.querySelector("h2").style.textDecoration = "underline";
+      }}
+      onMouseLeave={(e) => { 
+        e.currentTarget.style.transform = "scale(1)"
+        e.currentTarget.querySelector("h2").style.textDecoration = "none";
+      }}
+      onClick={onClick}
+    >
+      <div className="bg-white p-6 shadow feature-card rounded-xl min-h-full">
+        <div className="relative">
+          {markdownify(profile.title, "h2", "text-center font-normal min-h-28" )}
+        </div>
+        <div className="mt-4 text-left">
+          {markdownify(`${profile.description.slice(0, 100)}...`, "p", "text-sm")}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProfilePopup({ profile, onClose }) {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-xl shadow-lg max-w-2xl w-full relative">
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 bg-red-500 text-white rounded-full w-8 h-8 flex items-center justify-center hover:bg-red-600"
+        >
+          &times;
+        </button>
+        <div>
+          {markdownify(profile.title, "h2", "text-center font-bold text-xl mb-4" )}
+          {markdownify(profile.description, "p", "text-justify" )}
+          <ul className="p-8 list-disc">
+            {profile.skills.map((skill, index) => (
+              <li key={index} className="">
+                {markdownify(skill.skill, "span", { className: "text-center font-normal" })}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PerksSection({ perkTitle, perks }) {
+  return (
+    <div className="bg-white p-12 shadow feature-card rounded-xl flex flex-col justify-center items-center mt-4">
+      <div className="relative">
+        {markdownify(perkTitle, "h2", "text-center font-normal" )}
+      </div>
+      <ul className="career-ul p-8">
+        {perks.map((perk, index) => (
+          <li key={index} className="p-2">
+            {markdownify(perk.perk, "span", { className: "text-center font-normal" })}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function ApplicationForm({ applyNow, fields, selectedFile, handleFileChange }) {
+  const { name, email, selected_file, message, submit, upload_cv } = fields;
+
+  return (
+    <div className="bg-white p-12 shadow feature-card rounded-xl mt-4">
+      <div className="relative mb-4">
+        {markdownify(applyNow, "h2", "text-center font-normal" )}
+      </div>
+      <form
+        className="flex flex-col gap-4"
+        method="POST"
+        data-netlify="true"
+        name="Career"
+        encType="multipart/form-data"
+      >
+        <input type="hidden" name="form-name" value="Career" />
+        <input
+          type="text"
+          name="name"
+          placeholder={name}
+          className="p-4 border border-gray-300 rounded-md"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder={email}
+          className="p-4 border border-gray-300 rounded-md"
+          required
+        />
+        <textarea
+          placeholder={message}
+          className="p-4 border border-gray-300 rounded-md"
+          rows="4"
+          name="message"
+          required
+        />
+        <label className="flex items-center justify-center p-4 border border-gray-300 rounded-md text-lg font-semibold text-gray-700 cursor-pointer bg-gray-100 hover:bg-gray-200 transition">
+          {upload_cv}
+          <input
+            type="file"
+            name="cv"
+            required
+            hidden
+            onChange={handleFileChange}
+            aria-label={selected_file}
+          />
+        </label>
+        {selectedFile && (
+          <p className="text-gray-600 text-sm mt-2">
+            {selected_file}: {selectedFile}
+          </p>
+        )}
+        <button type="submit" className="btn btn-primary">
+          {submit}
+        </button>
+      </form>
     </div>
   );
 }
